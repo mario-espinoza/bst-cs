02837997e71ba07a7fe5b8a03bac8a006194d189
mae7 <mario.espinoza@equifax.com>
2018-12-25 11:16:01 -0300

    updated tests

---
+++ BST.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace BST.Tests
+{
+    public class UnitTest1
+    {
+        private readonly Tree typedReference; 
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
